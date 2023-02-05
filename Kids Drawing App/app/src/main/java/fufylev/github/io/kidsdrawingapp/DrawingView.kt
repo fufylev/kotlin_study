bd66baef4951efa2be96a3d894447e04de489e42
Andrei Fufylev <fufylev@gmail.com>
2023-02-05 15:29:22 +0500

    created brush size UI
    created brush size UI

--- Kids Drawing App/app/src/main/java/fufylev/github/io/kidsdrawingapp/DrawingView.kt
+++ Kids Drawing App/app/src/main/java/fufylev/github/io/kidsdrawingapp/DrawingView.kt
@@ -3,6 +3,7 @@ package fufylev.github.io.kidsdrawingapp
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 
@@ -29,7 +30,7 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
         mDrawPaint!!.strokeJoin = Paint.Join.ROUND
         mDrawPaint!!.strokeCap = Paint.Cap.ROUND
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
-        mBrushSize = 20.toFloat()
+//        mBrushSize = 20.toFloat()
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -89,7 +90,15 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
         return true
     }
 
-    internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path() {
+    fun setSizeForBrush(newSize: Float) {
+        mBrushSize = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            newSize,
+            resources.displayMetrics,
+        )
 
+        mDrawPaint!!.strokeWidth = mBrushSize
     }
+
+    internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path() {}
 }
--- Kids Drawing App/app/src/main/java/fufylev/github/io/kidsdrawingapp/MainActivity.kt
+++ Kids Drawing App/app/src/main/java/fufylev/github/io/kidsdrawingapp/MainActivity.kt
@@ -1,11 +1,51 @@
 package fufylev.github.io.kidsdrawingapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Dialog
 import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private var drawingView: DrawingView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawingView = findViewById(R.id.drawing_view)
+        drawingView?.setSizeForBrush(10.toFloat())
+
+        val brushBtn: ImageButton = findViewById(R.id.ib_brush)
+        brushBtn.setOnClickListener {
+            showBrushSizeChoiceDialog()
+        }
+    }
+
+    private fun showBrushSizeChoiceDialog() {
+        val brushDialog = Dialog(this)
+
+        brushDialog.setContentView(R.layout.dialog_brash_size)
+        brushDialog.setTitle("Brush size")
+
+        val smallBtn: ImageButton = brushDialog.findViewById(R.id.id_small_brush)
+        smallBtn.setOnClickListener {
+            drawingView?.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        }
+
+        val mediumBtn: ImageButton = brushDialog.findViewById(R.id.id_medium_brush)
+        mediumBtn.setOnClickListener {
+            drawingView?.setSizeForBrush(20.toFloat())
+            brushDialog.dismiss()
+        }
+
+        val bigBtn: ImageButton = brushDialog.findViewById(R.id.id_big_brush)
+        bigBtn.setOnClickListener {
+            drawingView?.setSizeForBrush(30.toFloat())
+            brushDialog.dismiss()
+        }
+
+        brushDialog.show()
     }
 }
