da05ce175ff6760e9e79ea96d51b6fc787f6280f
Andrei Fufylev <fufylev@gmail.com>
2023-03-13 11:44:57 +0500

    Image and button added

--- 7MinutesWorkOut/app/src/main/java/fufylev/github/io/a7minutesworkout/MainActivity.kt
+++ 7MinutesWorkOut/app/src/main/java/fufylev/github/io/a7minutesworkout/MainActivity.kt
@@ -1,11 +1,13 @@
 package fufylev.github.io.a7minutesworkout
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
